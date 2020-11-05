810ebcddaa0bf0b09c807b7d1148f18c82e49873
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-05 03:37:07 -0500

    First Day Changes

--- LAB08_BRYANHUGHES_SECTIONB.sql
+++ LAB08_BRYANHUGHES_SECTIONB.sql
@@ -4,3 +4,27 @@ CREATE TABLE librarian_info (
 	librarian_lname 	VARCHAR(255)
 );
 
+CREATE TABLE librarian_schedule (
+	shift			INT		PRIMARY KEY,
+	shift_day		DATE	NOT NULL,
+	librarian		INT		REFERENCES librarian_info(librarian_id)	
+);
+
+ALTER TABLE librarian_schedule
+ADD COLUMN assignment_id INT
+	REFERENCES librarian_assignments(assignment_id);
+
+
+CREATE TABLE librarian_assignments (
+	assignment_id	INT				PRIMARY KEY,
+	assignment_name VARCHAR(255)	NOT NULL,
+	librarian		INT		
+		REFERENCES librarian_info(librarian_id),
+	shift			INT		
+		REFERENCES librarian_schedule(shift)
+);
+
+
+
+
+
